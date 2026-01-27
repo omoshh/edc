884ed580899185f682f096330eb42ee835e00552
sanya <omoshh@protonmail.com>
2026-01-27 15:00:27 +0200

    add swagger schemas and endpoint descriptions

--- backend/api.py
+++ backend/api.py
@@ -8,6 +8,9 @@ import threading
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 from contextlib import asynccontextmanager
+from pydantic import BaseModel
+from datetime import datetime
+from typing import List
 
 from db.database import get_conn, get_name
 from db.setup_db import init_db
@@ -32,7 +35,22 @@ async def lifespan(app: fastapi.FastAPI):
     
     print("Shutting down...")
 
-app = fastapi.FastAPI(lifespan=lifespan)
+class Metric(BaseModel):
+    timestamp: datetime
+    cpu_usage: float
+    ram_usage: float
+    load_average: float
+    network_bandwidth: float
+
+class MetricRangeResponse(BaseModel):
+    data: List[Metric]
+
+app = fastapi.FastAPI(
+    title="Metrics API",
+    description="API for getting metrics from database",
+    version="1.0.0",
+    lifespan=lifespan
+    )
 
 load_dotenv()
 host = os.getenv('API_HOST', '0.0.0.0')
@@ -45,24 +63,42 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/metrics")
+@app.get("/metrics",
+    summary="Last metric",
+    description="Returns last entry from database",
+    response_model=Metric
+    )
 def get_metrics():
     name = get_name()
     query = f"""
-                SELECT * FROM {name} 
-                ORDER BY timestamp DESC 
-                LIMIT 1
+                SELECT timestamp, cpu_usage, ram_usage, load_average, network_bandwidth FROM {name} 
+                ORDER BY timestamp 
+                DESC LIMIT 1
             """
     try:
         with get_conn() as conn:
             with conn.cursor() as cur:
                 cur.execute(query)
-                return cur.fetchone()
+                r = cur.fetchone()
+                if not r:
+                    raise fastapi.HTTPException(404, "No data")
+                return {
+                    "timestamp": r["timestamp"],
+                    "cpu_usage": r["cpu_usage"],
+                    "ram_usage": r["ram_usage"],
+                    "load_average": r["load_average"],
+                    "network_bandwidth": r["network_bandwidth"],
+                }
     except Exception as e:
         print(f"Database error: {e}")
-        return None
+        print(f"API Detailed Error: {e}") 
+        raise fastapi.HTTPException(500, detail=str(e))
 
-@app.get("/metrics/range")
+@app.get("/metrics/range",
+    summary="Metrics from a period",
+    description="Returns metrics from start to end",
+    response_model=MetricRangeResponse
+    )
 def get_metrics_range(start: str, end: str):
     table = get_name()
     query = f"SELECT * FROM {table} WHERE timestamp BETWEEN %s AND %s ORDER BY timestamp ASC"
@@ -70,11 +106,23 @@ def get_metrics_range(start: str, end: str):
         with get_conn() as conn:
             with conn.cursor() as cursor:
                 cursor.execute(query, (start, end))
-                data = cursor.fetchall()
-                return {"data": data}
+                rows = cursor.fetchall()
+
+                return {
+                    "data": [
+                        {
+                            "timestamp": r["timestamp"],
+                            "cpu_usage": r["cpu_usage"],
+                            "ram_usage": r["ram_usage"],
+                            "load_average": r["load_average"],
+                            "network_bandwidth": r["network_bandwidth"],
+                        }
+                        for r in rows
+                    ]
+                }
     except Exception as e:
         print(f"Database error: {e}")
-        return None
+        return {"data": []}
 
 if __name__ == "__main__":
     host = os.getenv("API_HOST", "0.0.0.0")
